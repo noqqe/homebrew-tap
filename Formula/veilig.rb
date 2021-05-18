9815ff21f33a4329c95d349d7bb24335dbbceb4f
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-18 11:31:38 +0200

    Brew formula update for veilig version v0.0.3

--- Formula/veilig.rb
+++ Formula/veilig.rb
@@ -5,25 +5,25 @@
 class Veilig < Formula
   desc "veilig - written in go"
   homepage "https://github.com/noqqe/veilig"
-  version "0.0.2"
+  version "0.0.3"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/noqqe/veilig/releases/download/v0.0.2/veilig_0.0.2_Darwin_x86_64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "63234e17a469a00f8301d66b78d4f13113845f7822393ae4a48095f45f97cf32"
+    url "https://github.com/noqqe/veilig/releases/download/v0.0.3/veilig_0.0.3_Darwin_x86_64.tar.gz", :using => CurlDownloadStrategy
+    sha256 "fe33143d9d0407c647ae82878f39b45647dd54eaa5bc2ca8f0f9acb30b4ac22f"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/noqqe/veilig/releases/download/v0.0.2/veilig_0.0.2_Darwin_arm64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "ae14bb65743f68b5d5429b8c1098d722bc0d98a5fafe1177008261dabf9f3b1b"
+    url "https://github.com/noqqe/veilig/releases/download/v0.0.3/veilig_0.0.3_Darwin_arm64.tar.gz", :using => CurlDownloadStrategy
+    sha256 "189ed723a8394f2ad8e70c80c41f452244fc70b9a43066c432170bc172c4abb3"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/noqqe/veilig/releases/download/v0.0.2/veilig_0.0.2_Linux_x86_64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "f4ff6e1921e4127dc6493ef0a1b2253a423a8d39c9b36367e334d19298c41b85"
+    url "https://github.com/noqqe/veilig/releases/download/v0.0.3/veilig_0.0.3_Linux_x86_64.tar.gz", :using => CurlDownloadStrategy
+    sha256 "edb791fb7bf2bbed70234a4d9d4976ebc3584a9d372e155c2a5f96acd84cd3d4"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/noqqe/veilig/releases/download/v0.0.2/veilig_0.0.2_Linux_arm64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "cb362894c7101593be7cad438a73ac5908fa46019afa9a67a8919d3d81752643"
+    url "https://github.com/noqqe/veilig/releases/download/v0.0.3/veilig_0.0.3_Linux_arm64.tar.gz", :using => CurlDownloadStrategy
+    sha256 "f3e75da9399f6dc211fbf525baa093402fcea432d91528585e85345a10de2f82"
   end
 
   conflicts_with "veilig"
